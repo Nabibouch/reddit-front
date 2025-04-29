570e4e13180f50bab3ca315a39180d1eaf2fce24
Nabibouch <lmrb.nabil@gmail.com>
2025-04-29 15:02:20 +0200

    first commit

---
+++ front/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Test from './component/Test'
+
+function App() {
+  
+
+  return (
+    <>
+      <p className='text-red-500'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet atque expedita repellat odit corrupti. 
+        Blanditiis rerum explicabo exercitationem magnam fugit repellat quidem, ipsum repudiandae distinctio.
+      </p>
+      <Test />
+    </>
+  )
+}
+
+export default App
