207612b0145e6156305ca976ab427ea03f5f279c
cpc <ilyaselaz2004@gmail.com>
2025-04-29 23:46:45 +0200

    Page d'inscription sans typo

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import Test from './component/Test'
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './Inscription/SignUp';
 
-function App() {
-  
-
+function SuccessPage() {
   return (
-    <>
-      <p className='text-red-500'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet atque expedita repellat odit corrupti. 
-        Blanditiis rerum explicabo exercitationem magnam fugit repellat quidem, ipsum repudiandae distinctio.
-      </p>
-      <Test />
-    </>
-  )
+    <div className="min-h-screen bg-black flex justify-center items-center text-white font-[Manrope] text-2xl">
+      Inscription réussie !
+    </div>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/Inscription" element={<SignUp />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
+  );
+}
