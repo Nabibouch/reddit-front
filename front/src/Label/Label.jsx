68d025b582f713efb419c174664bf4b83ca955bf
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-05-06 11:30:48 +0200

    creation fichier post

--- front/src/Label/Label.jsx
+++ front/src/Label/Label.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { X } from 'lucide-react'
 
 export  default function  LabelButton() {
-    
         return (
             <button
                  className={'inline-flex items-center gap-2 px-4 py-1 rounded-full bg-gray-100 text-gray-800 border border-transparent hover:border-gray-500 transition-colors duration-200'}
--- front/src/Label/LabelWIcon.jsx
+++ front/src/Label/LabelWIcon.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { X } from 'lucide-react';
 
-export default function LabelButtonWithIcon({ text = "Label", onRemove }) {
+export default function LabelButtonWithIcon({ text = "Label"}) {
   const [visible, setVisible] = useState(true);
 
   if (!visible) return null;
@@ -10,8 +10,7 @@ export default function LabelButtonWithIcon({ text = "Label", onRemove }) {
     <div className="group inline-flex items-center gap-2 px-4 py-1 rounded-full bg-gray-100 text-gray-800 border border-transparent hover:border-gray-500 transition-colors duration-200">
       <button
         onClick={() => {
-          setVisible(false);
-          if (onRemove) onRemove(); 
+          setVisible(false); 
         }}
         className="flex items-center justify-center rounded-full p-1 bg-gray-300 text-gray-900 group-hover:bg-gray-400"
       >
