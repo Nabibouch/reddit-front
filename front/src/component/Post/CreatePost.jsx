ada70a21201960e6ee6482f41d9f0828f80f7ef1
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-05-07 10:31:49 +0200

    ajout image dispo

--- front/src/component/Post/CreatePost.jsx
+++ front/src/component/Post/CreatePost.jsx
@@ -3,8 +3,14 @@ import requiredOrNot from '../Input with label/Input'
 import PrimaryButton from '../Button/PrimaryButton' 
 import Input from '../Input with label/Input'
 import { Plus } from 'lucide-react'
+import { useRef } from 'react'
 
 export default function CreatePost() {
+    const fileInputRef = useRef(null)
+    
+    const handleUploadClick = () => {
+        fileInputRef.current.click()
+    }
     return (
         <div className="bg-[#161B21] p-6 rounded-2xl w-fit">
             <div className='flex flex-col gap-4 p-6 rounded-2xl'>
@@ -14,17 +20,14 @@ export default function CreatePost() {
             
                 <div className='w-[516px] border border-[#9ACECA] px-3 rounded-[50px] h-[200px] flex items-center justify-center'>
                     <h2 className="text-[#9ACECA]">Add Media</h2>
-                    <div className="w-5 h-5 rounded-full bg-white flex items-center justify-center cursor-pointer">
+                    <div  onClick={handleUploadClick} className="w-5 h- rounded-full bg-white flex items-center justify-center cursor-pointer">
                         < Plus className="text-black w-5 h-5" />
                     </div>
+                    <input type="file" accept="image/*,video/*" ref={fileInputRef} className="hidden" />
                 </div>
 
                 <Input titre="Description" required={true} />
 
-                {/* Add Media
-                <div onClick={handleClick} className='w-[516px] h-[200px] border border-white rounded-[14px] flex items-center justify-center cursor-pointer bg-[#2c2c2c] text-[#9ACECA]'>
- 
-                 </div> */}
                 <div className='flex gap-4'>
                     <PrimaryButton title="Annuler" />
                     <PrimaryButton title="Poster" />
