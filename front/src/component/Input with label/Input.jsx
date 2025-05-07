2b52ec28352da6bd06e9a920345185b3a4f0b190
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-05-07 10:18:40 +0200

    post quasi fini

--- front/src/component/Input with label/Input.jsx
+++ front/src/component/Input with label/Input.jsx
@@ -9,9 +9,10 @@ const Input = ({titre, required=false}) => {
 
     return(
         <div className="flex flex-col gap-2">
-            <h2>{titre}{requiredOrNot()}</h2>
+            <h2 className="text-[#9ACECA]">{titre}{requiredOrNot()}</h2>
             {/* <input type="text" className="w-[516px] border border-white px-2 rounded-[14px]" /> */}
-            <textarea className="w-[516px] border border-black px-3 rounded-[14px] h-[30px]" name="text" id="text"></textarea>
+            <textarea className="w-[516px] border border-[#9ACECA] px-3 rounded-[14px] h-[30px]" name="text" id="text"></textarea>
+
         </div>
     )
 }
--- front/src/component/Post/CreatePost.jsx
+++ front/src/component/Post/CreatePost.jsx
@@ -2,27 +2,33 @@ import { useState } from 'react'
 import requiredOrNot from '../Input with label/Input'
 import PrimaryButton from '../Button/PrimaryButton' 
 import Input from '../Input with label/Input'
+import { Plus } from 'lucide-react'
 
 export default function CreatePost() {
     return (
-        <div className='flex flex-col gap-4'>
-            {/* Titre */}
-            <Input titre="Titre" required={true} />
-                <div className='flex flex-col gap-2'> </div>
+        <div className="bg-[#161B21] p-6 rounded-2xl w-fit">
+            <div className='flex flex-col gap-4 p-6 rounded-2xl'>
+                {/* Titre */}
+                 <Input titre="Titre" required={true} />
+                    <div className='flex flex-col gap-2'> </div>
             
-            <div className='w-[516px] border border-black px-3 rounded-[50px] h-[200px]'>
-                 <h2>Add Media</h2>
-            </div>
+                <div className='w-[516px] border border-[#9ACECA] px-3 rounded-[50px] h-[200px] flex items-center justify-center'>
+                    <h2 className="text-[#9ACECA]">Add Media</h2>
+                    <div className="w-5 h-5 rounded-full bg-white flex items-center justify-center cursor-pointer">
+                        < Plus className="text-black w-5 h-5" />
+                    </div>
+                </div>
 
-            <Input titre="Description" required={true} />
+                <Input titre="Description" required={true} />
 
-           {/* Add Media
-            <div onClick={handleClick} className='w-[516px] h-[200px] border border-white rounded-[14px] flex items-center justify-center cursor-pointer bg-[#2c2c2c] text-[#9ACECA]'>
+                {/* Add Media
+                <div onClick={handleClick} className='w-[516px] h-[200px] border border-white rounded-[14px] flex items-center justify-center cursor-pointer bg-[#2c2c2c] text-[#9ACECA]'>
  
-            </div> */}
-            <div className='flex gap-4'>
-                <PrimaryButton title="Annuler" />
-                <PrimaryButton title="Poster" />
+                 </div> */}
+                <div className='flex gap-4'>
+                    <PrimaryButton title="Annuler" />
+                    <PrimaryButton title="Poster" />
+                </div>
             </div>
         </div>
     )
